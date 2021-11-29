f941b809fcc3c2203a1778e51bff780e7237febb
ArtyomKozyrev8 <kozirev8@gmail.com>
2021-11-29 16:32:18 +0300

    added test for update table rule

--- test_server/test.py
+++ test_server/test.py
@@ -2,10 +2,10 @@ from aiohttp import ClientSession
 import asyncio
 import json
 
-SERVER = "http://localhost:5879/"
+SERVER = "http://localhost:5879/api/v1/"
 
 SCHEMA = {
-    "schema_name": "u4",
+    "schema_name": "df",
     "x": {
         "y": {
             "y1": 10,
@@ -14,14 +14,16 @@ SCHEMA = {
         "z": 10,
     },
     "a": {
-        "b": 1,
-        "c": "xh1",
+        "ba": 1000,
+        "ca": "xh1",
     }
 }
 
+UPDATE_ID = 200
+
 
 async def create_new_json_schema(session: ClientSession) -> None:
-    url = f"{SERVER.rstrip('/')}/api/create_new_rule_schema"
+    url = f"{SERVER.rstrip('/')}/create_new_rule_schema"
     data = {
         "json_schema": json.dumps(SCHEMA),
     }
@@ -37,9 +39,27 @@ async def create_new_json_schema(session: ClientSession) -> None:
             print(f"Unexpected error {r.status}. {text}")
 
 
+async def update_existent_json_schema(session: ClientSession) -> None:
+    url = f"{SERVER.rstrip('/')}/update_rule_schema/{UPDATE_ID}"
+    data = {
+        "json_schema": json.dumps(SCHEMA),
+    }
+
+    async with session.post(url, data=data) as r:
+        if r.status == 200:
+            print(f"OK: {r.status}. Schema updated")
+        elif r.status == 400:
+            mes = await r.json()
+            print(f"Incorrect scheme error {r.status}. {mes}")
+        else:
+            text = await r.text('utf8')
+            print(f"Unexpected error {r.status}. {text}")
+
+
 async def main_wrapper():
     async with ClientSession() as session:
-        await create_new_json_schema(session)
+        # await create_new_json_schema(session)
+        await update_existent_json_schema(session)
 
 
 if __name__ == '__main__':
