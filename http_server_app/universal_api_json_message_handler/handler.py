c5eaf1a4e733ca8b6f1bb1a60db6c6ff0c311097
ArtyomKozyrev8 <kozirev8@gmail.com>
2021-12-06 13:17:44 +0300

    added asyncio.gather to await multiple coroutines simultaneously

--- http_server_app/universal_api_json_message_handler/handler.py
+++ http_server_app/universal_api_json_message_handler/handler.py
@@ -6,7 +6,7 @@ We do it due to eval function rules, check global, local scope rules for eval in
 """
 
 import asyncio
-from typing import Dict, Any, Callable
+from typing import Dict, Any, Callable, Coroutine, Tuple
 from http_server_app.user_api_predefined_functions.some_easy_func import (
     power_3_minus_1,
     square,
@@ -20,7 +20,7 @@ async def process_data_based_on_rule_schema(
         rule_schema: Dict[Any, Any],
 ) -> Dict[Any, Any]:
     """
-    Process some json dictionary-like message in accordance with scheme provided by user
+    Process some json dictionary-like message in accordance with schema provided by user
     :param json_data: some json dictionary-like message
     :param rule_schema: user defined scheme to decode the message
     :return: result of decoding
@@ -28,10 +28,16 @@ async def process_data_based_on_rule_schema(
     r = dict()  # storage for results
 
     def _make_func(create_outer_main_func_code) -> Callable:
-        """Creates function from user-defined scheme code"""
+        """Creates function from user-defined schema code"""
         global_dict = {}  # global var scope dictionary
         exec(create_outer_main_func_code, global_dict)  # compile code from user defined json scheme
         return global_dict['outer_main']  # get function object for further use in outer code
+
+    async def _coroutine_return_dict_key_wrapper(key: Any, coroutine: Coroutine) -> Tuple[Any, Any]:
+        """Some _get_key_result results value could be coroutines instead of final results. So we have
+        to wrap these coroutine in order to know key, val pairs and then rewrite results in r dictionary"""
+        result = await coroutine
+        return key, result
 
     async def _get_key_result(
             _json_data: Dict[Any, Any],
@@ -43,18 +49,18 @@ async def process_data_based_on_rule_schema(
             for i in _schema.keys():
                 await _get_key_result(_json_data[i], _schema[i], "_".join([key, i]))
         else:
-            if 'def' not in _schema:  # lambda or predefined function (in app code)
-                temp = (eval(f"{_schema}(_json_data)"))
-                if asyncio.iscoroutine(temp):
-                    r[key.lstrip("_")] = await temp  # async predefined function
-                else:
-                    r[key.lstrip("_")] = temp  # sync predefined function or lambda function result
-            else:  # sync or async function defined in rule schema
-                if "async" not in _schema:
-                    r[key.lstrip("_")] = _make_func(_schema)(_json_data)
-                else:
-                    r[key.lstrip("_")] = await _make_func(_schema)(_json_data)
+            if 'def' not in _schema:  # lambda or predefined sync or async function (in app code)
+                r[key.lstrip("_")] = (eval(f"{_schema}(_json_data)"))
+            else:  # sync or async function defined in rule schema (by user in GUI)
+                r[key.lstrip("_")] = _make_func(_schema)(_json_data)
 
     await _get_key_result(json_data, rule_schema)  # call inner helper function
+
+    # await coroutines which could be in results
+    key_vs_cor_res = [_coroutine_return_dict_key_wrapper(k, v) for k, v in r.items() if asyncio.iscoroutine(v)]
+    res = await asyncio.gather(*key_vs_cor_res)
+    # substitute coroutines with values
+    for k, v in res:
+        r[k] = v
 
     return r
